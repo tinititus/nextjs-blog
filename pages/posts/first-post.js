7c3ff6df7bd5557d9eaa60d46baae3df179d1752
Tito <tito.carvalho@fdte.io>
2021-09-25 01:45:36 -0300

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Doloremque, quisquam.
+      </p>
+    </Layout>
+  );
+}
