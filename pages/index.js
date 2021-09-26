44e918c95b2ad8c7b96e9f445816e5f507045a58
Tito <tito.carvalho@fdte.io>
2021-09-26 15:45:36 -0300

    update <p>

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hello, my name is Tito. I'm a software engineer.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
